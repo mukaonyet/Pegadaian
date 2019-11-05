bf72dbd519f90729d02d934cb914c7f85c566323
mukaontet <danangibnu90@gmail.com>
2019-11-05 13:38:07 +0700

    Update DanangIA.java
    Ada Pull gak??

--- Tugas_Java_Danang_Ibnu/Danang/src/PegadaianGit/DanangIA.java
+++ Tugas_Java_Danang_Ibnu/Danang/src/PegadaianGit/DanangIA.java
@@ -5,6 +5,7 @@ public class DanangIA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Danang versi 3");
+		System.out.println("Ada Pull gak");
 	}
 
 }
